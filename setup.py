b28617d5f2fe71c5484ec1b678c50a429a0b5b56
Arun K Singh <22137632+arunksingh16@users.noreply.github.com>
2021-10-08 11:00:01 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+ name='helloworld-aks',
+ version='0.0.1',
+ description='Hello World Demo'
+ long_description=long_description(),
+ long_description_content_type='text/markdown',
+ project_urls={
+   'GitHub': 'https://github.com/arunksingh16/pypi-package-ex'
+    },
+)
